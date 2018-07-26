87a6058c782bee5a8dda3ad0231802c76638a39f
Rakesh Kumar Shardiwal <rakesh.shardiwal@gmail.com>
2018-07-26 10:49:06 +0530

    Timeline option in-progress

--- protected/views/patients/_stats_overview.php
+++ protected/views/patients/_stats_overview.php
@@ -70,6 +70,21 @@
     <div class="card-header" id="headingOne">
       <h5 class="mb-0">
         <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          TimeLine
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+            DATE FILTER
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
           Demographic
         </button>
       </h5>
